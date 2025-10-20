2ed614c67d972d3a02a35ae1dc03d14d08ab6761
Wilksy <a.wilksy92@gmail.com>
2025-10-20 20:03:12 +1000

    test(index): add TODO for centralized test exports

--- backend/__tests__/index.ts
+++ backend/__tests__/index.ts
@@ -1,2 +1,6 @@
 export * from "./testHelpers.ts";
 export * from "./serverTestHelper.ts";
+
+// TODO: Export all test files for centralized test running
+// export * from "./shared/schemas/api/people.test.ts";
+// ... etc
